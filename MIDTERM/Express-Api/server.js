6c020d550f3a6160d0f74b0bbd276192076243de
Luis Gabriel Narciso Aquino <thisisgab23@gmail.com>
2025-08-15 12:13:15 +0800

    Add files via upload

---
+++ MIDTERM/Express-Api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/data', (req, res) => {
+    res.json({ message: 'This is JSON data', time: new Date() });
+});
+
+
+app.get('/greet', (req, res) => {
+    const name = req.query.name || 'Guest';
+    res.json({
+        message: `Hello, ${name}!`,
+        date: new Date(),
+        info: 'Some resource used with personalized using query parameters.'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+
